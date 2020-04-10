d970bb4cd48338cea0d9ae89386ee1c61998924f
Tien-Yi Chi <r07922097@ntu.edu.tw>
2020-04-10 17:04:19 +0800

    Improve indentation

--- fork.c
+++ fork.c
@@ -22,8 +22,7 @@ PROC_INFO procs[20];
 int check_finished(PROC_INFO* procs, int len)
 {
     int finished = 1;
-    for(int i = 0; i < len; i++)
-    {
+    for(int i = 0; i < len; i++) {
         finished *= procs->status;
     }
 
@@ -35,8 +34,7 @@ void main(void)
     int    num_procs;
     char   buf[BUF_SIZE];
     char   policy[10];
-    for(int i = 0; i < 20; i++)
-    {
+    for(int i = 0; i < 20; i++) {
         procs[i].pid = -1;
         procs[i].status = 1;
     }
@@ -45,8 +43,7 @@ void main(void)
     printf("Policy: %s\n", policy);
     printf("Number of processes: %d\n", num_procs);
 
-    for(int i = 0; i < num_procs; i++)
-    {
+    for(int i = 0; i < num_procs; i++) {
         scanf("%s %d %d", procs[i].name, &procs[i].t_start, &procs[i].t_exec);
         procs[i].status = 0;
         printf("Name: %s, start at: %d, exec for %d\n", procs[i].name, procs[i].t_start, procs[i].t_exec);
@@ -74,24 +71,19 @@ void main(void)
     long long num_quantum = 0;
     long long old_num_quantum = 0;
 
-    do
-    {
+    do {
         gettimeofday(&ts_main, NULL);
         ll_tsm = ts_main.tv_sec*1000LL + ts_main.tv_usec/1000;
         num_quantum = (ll_tsm - ll_ts) / ll_t_quantum;
-        for(int i = 0; i < num_procs; i++)
-        {
-            if(procs[i].pid < 0 && old_num_quantum < procs[i].t_start && num_quantum > procs[i].t_start)
-            {
+        for(int i = 0; i < num_procs; i++) {
+            if(procs[i].pid < 0 && old_num_quantum < procs[i].t_start && num_quantum > procs[i].t_start) {
                 pid_t pid = fork();
-                switch (pid)
-                {
+                switch (pid) {
                     case 0:
                         // TODO: sys call and use printk
                         {
                             volatile unsigned long j, k;
-                            for(k=0;k<procs[i].t_exec;k++)
-                            {
+                            for(k=0;k<procs[i].t_exec;k++) {
                                 for(j=0;j<1000000UL;j++);
                             }
                         }
@@ -103,14 +95,10 @@ void main(void)
                         procs[i].pid = pid;
                         break;
                 }
-
-            }
-            else if(procs[i].pid > 0)
-            {
+            } else if(procs[i].pid > 0) {
                 // TODO: Check pid status; if finished, set procs[i].status = 0
             }
         }
-
     } while(!check_finished(procs, num_procs));
 
     return;
