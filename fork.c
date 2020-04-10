8daa743c85456d4ef0e8a8515d5264b8e9df6ff7
Tien-Yi Chi <r07922097@ntu.edu.tw>
2020-04-10 16:55:50 +0800

    A empty prototype

--- fork.c
+++ fork.c
@@ -8,15 +8,51 @@
 #define MAX_COUNT 50
 #define BUF_SIZE 100
 
+typedef struct proc_info
+{
+    char name[10];
+    pid_t pid;
+    int t_start;
+    int t_exec;
+    int status;
+} PROC_INFO;
+
+PROC_INFO procs[20];
+
+int check_finished(PROC_INFO* procs, int len)
+{
+    int finished = 1;
+    for(int i = 0; i < len; i++)
+    {
+        finished *= procs->status;
+    }
+
+    return finished;
+}
+
 void main(void)
 {
-    pid_t  pid;
-    int    i;
-    double diff_t;
+    int    num_procs;
     char   buf[BUF_SIZE];
+    char   policy[10];
+    for(int i = 0; i < 20; i++)
+    {
+        procs[i].pid = -1;
+        procs[i].status = 1;
+    }
+    scanf("%s", policy);
+    scanf("%d", &num_procs);
+    printf("Policy: %s\n", policy);
+    printf("Number of processes: %d\n", num_procs);
+
+    for(int i = 0; i < num_procs; i++)
+    {
+        scanf("%s %d %d", procs[i].name, &procs[i].t_start, &procs[i].t_exec);
+        procs[i].status = 0;
+        printf("Name: %s, start at: %d, exec for %d\n", procs[i].name, procs[i].t_start, procs[i].t_exec);
+    }
 
     struct timeval te, ts;
-    printf("PID: %d", getpid());
     gettimeofday(&ts, NULL);
     long long mts = ts.tv_sec*1000LL + ts.tv_usec/1000;
     {
@@ -28,34 +64,80 @@ void main(void)
     }
     gettimeofday(&te, NULL); // get current time
     long long mte = te.tv_sec*1000LL + te.tv_usec/1000;
-    printf("Unit time: %lld", mte - mts);
+    long long ll_t_quantum = mte - mts;
     fflush(stdout);
-    for(int k=0;k<5;k++)
+    
+    struct timeval ts_main;
+    gettimeofday(&ts_main, NULL);
+    long long ll_tsm = ts_main.tv_sec*1000LL + ts_main.tv_usec/1000;
+    long long ll_ts = ll_tsm;
+    long long num_quantum = 0;
+    long long old_num_quantum = 0;
+
+    do
     {
-        pid = fork();
-        switch (pid)
+        gettimeofday(&ts_main, NULL);
+        ll_tsm = ts_main.tv_sec*1000LL + ts_main.tv_usec/1000;
+        num_quantum = (ll_tsm - ll_ts) / ll_t_quantum;
+        for(int i = 0; i < num_procs; i++)
         {
-            case 0:
-                for (i = 1; i <= MAX_COUNT; i++) {
-                    sprintf(buf, "This line is from child pid %d, value = %d\n", getpid(), i);
-                    write(1, buf, strlen(buf));
+            if(procs[i].pid < 0 && old_num_quantum < procs[i].t_start && num_quantum > procs[i].t_start)
+            {
+                pid_t pid = fork();
+                switch (pid)
+                {
+                    case 0:
+                        // TODO: sys call and use printk
+                        {
+                            volatile unsigned long j, k;
+                            for(k=0;k<procs[i].t_exec;k++)
+                            {
+                                for(j=0;j<1000000UL;j++);
+                            }
+                        }
+                        // TODO: sys call and use printk
+                        exit(0);
+                        break;
+                    
+                    default:
+                        procs[i].pid = pid;
+                        break;
                 }
-                exit(0);
-                break;
-            
-            default:
-                for (i = 1; i <= MAX_COUNT; i++) {
-                    sprintf(buf, "This line is from pid %d, value = %d\n", getpid(), i);
-                    write(1, buf, strlen(buf));
-                }
-                break;
+
+            }
+            else if(procs[i].pid > 0)
+            {
+                // TODO: Check pid status
+            }
         }
-    }
+
+    } while(!check_finished(procs, num_procs));
+    // for(int k=0;k<5;k++)
+    // {
+    //     pid = fork();
+    //     switch (pid)
+    //     {
+    //         case 0:
+    //             for (i = 1; i <= MAX_COUNT; i++) {
+    //                 sprintf(buf, "This line is from child pid %d, value = %d\n", getpid(), i);
+    //                 write(1, buf, strlen(buf));
+    //             }
+    //             exit(0);
+    //             break;
+            
+    //         default:
+    //             for (i = 1; i <= MAX_COUNT; i++) {
+    //                 sprintf(buf, "This line is from pid %d, value = %d\n", getpid(), i);
+    //                 write(1, buf, strlen(buf));
+    //             }
+    //             break;
+    //     }
+    // }
     // wait(NULL);
 
-    if(pid > 0)
-    {
-        printf("Parent ends.\n");
-    }
+    // if(pid > 0)
+    // {
+    //     printf("Parent ends.\n");
+    // }
     return;
 }
