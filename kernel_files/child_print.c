cfcb72071379e75ce7b47b73daae82deb2a02ac3
TienYi Chi <r07922097@ntu.edu.tw>
2020-04-26 18:19:38 +0800

    int -> long int

--- kernel_files/child_print.c
+++ kernel_files/child_print.c
@@ -1,9 +1,9 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
-#include <time.h>
+#include <linux/time.h>
 
-asmlinkage void sys_done_running(char *name, int pid, const struct timespec *start, const struct timespec *end)
+asmlinkage void sys_done_running(char *name, int pid, const struct timespec start, const struct timespec end)
 {
-    printk("%s %d %d.%d %d.%d", name, pid, start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
+    printk("%s %d %ld.%ld %ld.%ld", name, pid, start.tv_sec, start.tv_nsec, end.tv_sec, end.tv_nsec);
     return;
 }
